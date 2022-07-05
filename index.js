424ebbd9539d4f485dd7fa1b362346e0b14a58b9
joeneedum <joeneedum@gmail.com>
2022-07-04 21:38:41 -0400

    sign in route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const userRoutes = require('./routes/user')
 const morgan = require('morgan')
 const bodyParser = require('body-parser'); // visual req.body in terminal
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator')
+
+
 // database
 connection()
 
@@ -13,6 +16,7 @@ connection()
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator())
 
 // routes
 app.use('/api',userRoutes);
@@ -22,51 +26,3 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Server is running on port! ${port}`)
 });
-
-// "use strict";
-
-// /**
-//  * Get unique error field name
-//  */
-// const uniqueMessage = error => {
-//     let output;
-//     try {
-//         let fieldName = error.message.substring(
-//             error.message.lastIndexOf(".$") + 2,
-//             error.message.lastIndexOf("_1")
-//         );
-//         output =
-//             fieldName.charAt(0).toUpperCase() +
-//             fieldName.slice(1) +
-//             " already exists";
-//     } catch (ex) {
-//         output = "Unique field already exists";
-//     }
-
-//     return output;
-// };
-
-/**
- * Get the erroror message from error object
- */
-// exports.errorHandler = error => {
-//     let message = "";
-
-//     if (error.code) {
-//         switch (error.code) {
-//             case 11000:
-//             case 11001:
-//                 message = uniqueMessage(error);
-//                 break;
-//             default:
-//                 message = "Something went wrong";
-//         }
-//     } else {
-//         for (let errorName in error.errorors) {
-//             if (error.errorors[errorName].message)
-//                 message = error.errorors[errorName].message;
-//         }
-//     }
-
-//     return message;
-// };
