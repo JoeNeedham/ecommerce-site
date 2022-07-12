73ed55ecc3dfe6645709667362e36cf256e65e43
joeneedum <joeneedum@gmail.com>
2022-07-11 22:17:36 -0400

    category routes done

--- routes/category.js
+++ routes/category.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { userById } = require('../controllers/user');
+const { userById } = require("../controllers/user");
 
-const { create } = require('../controllers/category');
+const { create, categoryById, read, update, remove, list } = require("../controllers/category");
 
-router.post('/category/create/:userId',
-    requireSignin,
-    isAuth,
-    isAdmin,
-    create 
-);
+router.get("/category/:categoryId", read);
+router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, update);
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, remove);
+router.get("/categories", list);
 
-router.param('userId', userById)
+router.param("categoryId", categoryById);
+router.param("userId", userById);
 
 module.exports = router;
