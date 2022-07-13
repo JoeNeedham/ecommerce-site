43d1d1de61f8ee038e9e4df8fc14944a213e3fa8
joeneedum <joeneedum@gmail.com>
2022-07-12 22:10:48 -0400

    routes almost done

--- routes/product.js
+++ routes/product.js
@@ -1,31 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update } = require('../controllers/product');
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo,
+} = require("../controllers/product");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
-router.get('/product/:productId', read)
-router.post('/product/create/:userId',
+router.get("/product/:productId", read);
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+    "/product/:productId/:userId",
     requireSignin,
     isAuth,
     isAdmin,
-    create 
+    remove
 );
-router.delete('/product/:productId/:userId',
+router.put(
+    "/product/:productId/:userId",
     requireSignin,
     isAuth,
     isAdmin,
-    remove 
+    update
 );
-router.put('/product/:productId/:userId',
-    requireSignin,
-    isAuth,
-    isAdmin,
-    update 
-);
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo);
 
-router.param('userId', userById);
-router.param('productId', productById);
+router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
