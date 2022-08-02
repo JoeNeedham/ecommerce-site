4184ed64091224e9bb06e7e8ba06cb3adb834f6d
joeneedum <joeneedum@gmail.com>
2022-08-02 17:53:59 -0400

    create client-side category route

--- client/src/admin/AddCategory.js
+++ client/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { createCategory } from './apiAdmin';
 
 const AddCategory = () => {
     const [name, setName] = useState('')
@@ -21,7 +22,15 @@ const AddCategory = () => {
         setError('')
         setSuccess(false)
         // make request to api to create category
-    }
+        createCategory(user._id, token, {name}).then(data =>{
+            if(data.error) {
+                setError(true);
+            } else {
+                setError('');
+                setSuccess(true);
+            }
+        });
+    };
 
     const newCategoryForm = () => (
         <form onSubmit={clickSubmit}>
@@ -32,12 +41,33 @@ const AddCategory = () => {
                     onChange={handleChange}
                     value={name}
                     autoFocus
+                    required
                 />
             </div>
             <button className='btn btn-outline-primary'>
                     Create Category
             </button>
         </form>
+    );
+
+    const showSuccess = () => {
+        if(success) {
+            return <h3 className='text-success'>{name} is created</h3>
+        }
+    };
+
+    const showError = () => {
+        if(error) {
+            return <h3 className='text-danger'> Category should be unique</h3>
+        }
+    }
+
+    const goBack = () => (
+        <div className="mt-5">
+            <Link to='/admin/dashboard' classname='text-warning'>
+                Back to Dashboard
+            </Link>
+        </div>
     );
 
     return (
@@ -47,7 +77,10 @@ const AddCategory = () => {
         >
             <div className="row">
                 <div className="col-md-8 offset-md-2">
+                    {showSuccess()}
+                    {showError()}
                     {newCategoryForm()}
+                    {goBack()}
                 </div>
             </div>
         </Layout>
--- server/models/category.js
+++ server/models/category.js
@@ -6,7 +6,8 @@ const categorySchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
-        maxLength: 34
+        maxLength: 34,
+        unique: true,
     },
 },
     { timestamps: true }
