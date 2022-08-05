315ea8f4b99b470f9bbdbc27adb4c4427cb80c56
joeneedum <joeneedum@gmail.com>
2022-08-05 19:19:13 -0400

    add product backend + style

--- client/src/admin/AddProduct.js
+++ client/src/admin/AddProduct.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
-import { createProduct} from './apiAdmin';
+import { createProduct, getCategories } from './apiAdmin';
 
 const AddProduct = () => {
     const [values, setValues] = useState({
@@ -38,9 +38,20 @@ const AddProduct = () => {
         formData
     } = values;
 
+    // load categories and set form data
+    const init = () => {
+        getCategories().then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error})
+            } else {
+                setValues({...values, categories: data, formData: new FormData()})
+            }
+        })
+    }
+
     useEffect(() => {
-        setValues({...values, formData: new FormData()})
-    }, [])
+        init();
+    }, []);
 
     const handleChange = name => event => {
         const value = name ===  'photo' ? event.target.files[0] : event.target.value
@@ -100,9 +111,10 @@ const AddProduct = () => {
                     onChange={handleChange('category')}
                     className='form-control'
                 >
-                    <option value="62e997f4ecda435b2a4a704b">Python</option>
-                    <option value="62e997f4ecda435b2a4a704b">PHP</option>
-
+                    <option>Please Select</option>
+                    {categories && categories.map((c, i) => (
+                        <option key={i} value={c._id}>{c.name}</option>
+                    ))}
                 </select>
             </div>
             <div className="form-group">
@@ -111,6 +123,7 @@ const AddProduct = () => {
                     onChange={handleChange('shipping')}
                     className='form-control'
                 >
+                    <option>Please Select</option>
                     <option value="0">No</option>
                     <option value="1">Yes</option>
                 </select>
@@ -123,13 +136,38 @@ const AddProduct = () => {
         </form>
     )
 
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    )
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{display: createdProduct ? '' : 'none'}}>
+            <h2>{`${createdProduct}`} is created!</h2>
+        </div>
+    )
+    
+    const showLoading = () => (
+        loading && (
+            <div className="alert alert-success">
+                <h2>Loading...</h2>
+            </div>
+        )
+    )
+
     return (
     <Layout
         title='Add a new product'
         description="Ready to add a new product?"
     >
         <div className="row">
-            <div className="col-md-8 offset-md-2">{newPostForm()}</div>
+            <div className="col-md-8 offset-md-2">
+                {showLoading()}
+                {showSuccess()}
+                {showError()}
+                {newPostForm()}  
+            </div>
         </div>
     </Layout>
     );
