3181c657efca37b27bb8b88e63d14dd0d900ccc3
joeneedum <joeneedum@gmail.com>
2022-08-06 23:35:41 -0400

    commit

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
+import { getProducts } from './apiCore';
 
 function Home() {
+    const [productsBySell, setProductsBySell] = useState([])
+    const [productsByArrival, setProductsByArrival] = useState([])
+    const [error, setError] = useState([])
+
     return (
         <Layout title="Home Page" description="Node React E-Commerce App">
             
