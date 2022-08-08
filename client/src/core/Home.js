ca6bbe43b7caeba839b96b6647702da3643210ba
joeneedum <joeneedum@gmail.com>
2022-08-07 23:11:07 -0400

    commit

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -7,9 +7,37 @@ function Home() {
     const [productsByArrival, setProductsByArrival] = useState([])
     const [error, setError] = useState([])
 
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else {
+                setProductsBySell(data)
+            }
+        });
+    }
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else {
+                setProductsByArrival(data)
+            }
+        });
+    }
+
+    useEffect(() => {
+        loadProductsByArrival()
+        loadProductsBySell()
+    },[])
+
     return (
         <Layout title="Home Page" description="Node React E-Commerce App">
-            
+            {JSON.stringify(productsBySell)}
+            <br/>
+            <br/>
+            {JSON.stringify(productsByArrival)}
         </Layout>
     )
 }
