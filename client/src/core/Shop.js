32da793f2de525c3f3a27bcff3d133468dc04d27
joeneedum <joeneedum@gmail.com>
2022-08-11 20:39:14 -0400

    commit

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -4,6 +4,7 @@ import Card from "./Card";
 import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
 import { prices } from "./fixedPrices";
+import RadioBox from "./RadioBox";
 
 const Shop = () => {
     const [myFilters, setMyFilters] = useState({
@@ -41,13 +42,20 @@ const Shop = () => {
         >
         <div className="row">
             <div className="col-4">
-            <h4>Filter by categories</h4>
-            <ul>
-                <Checkbox
-                categories={categories}
-                handleFilters={filters => handleFilters(filters, "category")}
-                />
-            </ul>
+                <h4>Filter by categories</h4>
+                <ul>
+                    <Checkbox
+                    categories={categories}
+                    handleFilters={filters => handleFilters(filters, "category")}
+                    />
+                </ul>
+                <h4>Filter by price range</h4>
+                <div>
+                    <RadioBox
+                    prices={prices}
+                    handleFilters={filters => handleFilters(filters, "price")}
+                    />
+                </div>
             </div>
 
             <div className="col-8">{JSON.stringify(myFilters)}</div>
