3f7dfa333e7dca19c3c3f05af73f0040e1e6db84
joeneedum <joeneedum@gmail.com>
2022-09-18 17:46:15 -0400

    commit

--- client/src/admin/AddProduct.js
+++ client/src/admin/AddProduct.js
@@ -27,13 +27,10 @@ const AddProduct = () => {
         description,
         price,
         categories,
-        category,
-        shipping,
         quantity,
         loading,
         error,
         createdProduct,
-        redirectToProfile,
         formData
     } = values;
 
@@ -47,7 +44,8 @@ const AddProduct = () => {
             }
         })
     }
-
+    
+// eslint-disable-next-line
     useEffect(() => {
         init();
     },[]);
